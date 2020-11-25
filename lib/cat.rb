bf75c0ff1b2a998ad575cbe0bbec6d9b82bb5bd6
baldo-vela <baldo.vela+github@gmail.com>
2020-11-25 12:30:24 -0600

    commit tree 3d50fd9513e257de7e914355c6979c6cc5b733d8

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
-  # code goes here
+    attr_accessor  :owner, :mood
+    attr_reader :name, :species
+
+    @@all= []
+
+    def initialize(name, owner)
+      @name = name
+      @species = "cat"
+      @owner = owner
+      @mood = "nervous"
+      save
+    end
+    
+    def save
+      @@all <<self
+    end
+
+    def self.all
+      return @@all
+    end
+    
 end
